91c2cc314ea08161d546fd347ed47cd43d1a6cad
Jaowat Raihan <raihanjaowat@outlook.com>
2018-07-26 11:48:08 +0600

    Commit 2

--- platforms/browser/www/js/app/controllers/student-controller.js
+++ platforms/browser/www/js/app/controllers/student-controller.js
@@ -10,5 +10,33 @@ angular.module('App').controller('ContactController', ['$scope', 'ContactService
     }, function (error) {
         console.log(error);
     });
+
+    $scope.result = 
+    [
+      {
+        courseName:"ENGLISH READING SKILLS & PUBLIC SPEAKING",
+        courseCode:"01488",
+        courseSection:"B3",
+        status:"Valid",
+        result:"A-",
+        marks:"86.00"
+      },
+      {
+        courseName:"DIFF CALCULUS AND COORDINATE GEOMETRY (MATHEMATICS 1) ",
+        courseCode:"01489",
+        courseSection:"B3",
+        status:"Valid",
+        result:"A+",
+        marks:"99.00"
+      },
+      {
+        courseName:"PHYSICS-1",
+        courseCode:"01490",
+        courseSection:"B3",
+        status:"Valid",
+        result:"A-",
+        marks:"86.00"
+      }
+    ];
  
 }]);
